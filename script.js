ae5ade3152d1c32ef6344d1cfdaa80a4ef2a853c
Lightz2002 <kenidyryan@gmail.com>
2021-09-05 12:26:30 +0700

    fix local storage bug

--- script.js
+++ script.js
@@ -1,145 +1,132 @@
 // Song Class: Represents a Song
 class Song {
-  constructor(singer, songName) {
-    this.singer = singer;
-    this.songName = songName;
-  }
+    constructor(singer, songName, url) {
+        this.singer = singer;
+        this.songName = songName;
+        this.url = url;
+    }
 }
 
 // UI Class : Handle UI Tasks
 class UI {
-  static displaySongs() {
-    const songs = Store.getSongs();
-    songs.forEach((song) => UI.addSongs(song));
-  }
+    static displaySongs() {
+        const songs = Store.getSongs();
 
-  static addSongs(song) {
-    const list = document.querySelector("#song-list");
-    const row = document.createElement("tr");
-
-    row.innerHTML = `
-      <td class="singer">${song.singer}</td>
-      <td class="songName">${song.songName}</td>
-      <td><i class="fas fa-trash delete"></i></td>
-    `;
-
-    list.appendChild(row);
-  }
-  static clearInputs() {
-    document.querySelector("#singer").value = "";
-    document.querySelector("#song-name").value = "";
-  }
-
-  static deleteSong(element) {
-    if (element.classList.contains("delete")) {
-      element.parentElement.parentElement.remove();
+        songs.forEach((song) => {
+            UI.AddSongToList(song);
+        });
     }
-  }
 
-  static searchSong(element) {
-    const singers = document.querySelectorAll(".singer");
-    const songNames = document.querySelectorAll(".songName");
-    singers.forEach((singer) => {
-      if (singer.innerText.indexOf(element) > -1 || singer.nextElementSibling.innerText.indexOf(element) > -1) {
-        singer.parentElement.style.display = "";
-      } else {
-        singer.parentElement.style.display = "none";
-      }
-    });
-  }
+    static AddSongToList(song) {
+        const list = document.querySelector("#song-list");
+        const row = document.createElement("tr");
 
-  static showAlert(message, className) {
-    const alert = document.createElement("div");
-    alert.className = `alert alert-${className}`;
-    alert.appendChild(document.createTextNode(message));
-    const container = document.querySelector(".container");
-    const form = document.querySelector("#music-form");
-    container.insertBefore(alert, form);
+        row.innerHTML = `
+        <td>${song.singer}</td>
+        <td>${song.songName}</td>
+        <td>${song.url}</td>
+        <td><button class='fas fa-trash bg-transparent border-0 text-white delete'></button></td>
+    `;
+        list.appendChild(row);
+    }
 
-    setTimeout(() => document.querySelector(".alert").remove(), 3000);
-  }
+    static clearInputs() {
+        document.querySelector("#singer").value = "";
+        document.querySelector("#song-name").value = "";
+        document.querySelector("#url").value = "";
+    }
+
+    static removeSong(e) {
+        if (e.classList.contains("delete")) {
+            e.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className) {
+        const div = document.createElement("div");
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector(".container");
+        const form = document.querySelector("#music-form");
+        container.insertBefore(div, form);
+
+        setTimeout(() => {
+            div.remove();
+        }, 3000);
+    }
 }
+
 // Store Class : Handles Storage
 class Store {
-  static getSongs() {
-    let songs;
-    if (localStorage.getItem("songs") === null) {
-      songs = [];
-    } else {
-      songs = JSON.parse(localStorage.getItem("songs"));
+    static getSongs() {
+        let songs;
+        if (localStorage.getItem("songs") === null) {
+            songs = [];
+        } else {
+            songs = JSON.parse(localStorage.getItem("songs"));
+        }
+        return songs;
     }
 
-    return songs;
-  }
+    static addSongs(song) {
+        let songs = Store.getSongs();
+        songs.push(song);
+        localStorage.setItem("songs", JSON.stringify(songs));
+    }
 
-  static addSongs(song) {
-    const songs = Store.getSongs();
-    songs.push(song);
-    localStorage.setItem("songs", JSON.stringify(songs));
-  }
-
-  static removeSongs(songName) {
-    const songs = Store.getSongs();
-    songs.forEach((song, index) => {
-      if (song.songName === songName) {
-        songs.splice(index, 1);
-      }
-    });
-
-    localStorage.setItem("songs", JSON.stringify(songs));
-  }
+    static removeSongs(url) {
+        let songs = Store.getSongs();
+        songs.forEach((song, index) => {
+            if (song.url === url) {
+                songs.splice(index, 1);
+            }
+        });
+        localStorage.setItem("songs", JSON.stringify(songs));
+    }
 }
 
 // Event : Display Songs
-document.addEventListener("DOMContentLoaded", UI.displaySongs);
+document.addEventListener("DOMContentLoaded", UI.displaySongs());
 
 // Event : Add A Song
-const form = document.querySelector("#music-form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+document.querySelector("#music-form").addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  const singer = document.querySelector("#singer").value;
-  const songName = document.querySelector("#song-name").value;
+    // Get Form Values
+    const singer = document.querySelector("#singer").value;
+    const songName = document.querySelector("#song-name").value;
+    const url = document.querySelector("#url").value;
 
-  // Validate
-  if (singer == "" || songName == "") {
-    UI.showAlert("Please fill in the inputs", "danger");
-  } else {
-    // Instantiate Song
-    const song = new Song(singer, songName);
+    // Validate
+    if (singer === "" || songName === "" || url === "") {
+        UI.showAlert("Please Fill In The Inputs", "danger");
+    } else {
+        // Instantiate A Song
+        const song = new Song(singer, songName, url);
 
-    // Add Songs To UI
-    UI.addSongs(song);
+        // Add Song To UI
+        UI.AddSongToList(song);
 
-    // Add Songs to Storage
-    Store.addSongs(song);
+        // Add Song To Store
+        Store.addSongs(song);
 
-    // Clear Inputs
-    UI.clearInputs();
+        // Clear Input
+        UI.clearInputs();
 
-    UI.showAlert("The Song Has Been Added", "success");
-  }
+        // Show Alert
+        UI.showAlert("The Song Has Been Successfully Added", "success");
+    }
 });
 
 // Event: Search A Song
-const searchForm = document.querySelector(".search-form");
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  let search = document.querySelector(".search").value;
-
-  // Search Song From UI
-  UI.searchSong(search);
-});
 
 // Event : Remove A Song
-// Remove Song From UI
 document.querySelector("#song-list").addEventListener("click", (e) => {
-  UI.deleteSong(e.target);
+    // Remove Song From UI
+    UI.removeSong(e.target);
+    // Remove Song From Store
+    Store.removeSongs(e.target.parentElement.previousElementSibling.textContent);
 
-  // Remove Song From Storage
-  Store.removeSongs(e.target.parentElement.previousElementSibling.textContent);
-
-  // Show Success Message
-  UI.showAlert("The Song Has Been Deleted", "success");
+    // Show Success Message
+    UI.showAlert("The Song Has Been Deleted", "success");
 });
